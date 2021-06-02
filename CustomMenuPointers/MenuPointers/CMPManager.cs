9db3d3430ecb6dab874d0887e109ffeaaaf96f07
dawnvt <#PC-modelling2>
2021-06-02 13:02:53 +0200

    Partial saber parenting - Thanks to @ToniMacaroni for explaining how to get proper CustomColor on sabers.

--- CustomMenuPointers/MenuPointers/CMPManager.cs
+++ CustomMenuPointers/MenuPointers/CMPManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using SaberFactory;
 using UnityEngine;
 using Zenject;
@@ -9,22 +11,29 @@ namespace CustomMenuPointers.MenuPointers
     {
         private readonly MenuSaberProvider _menuSaberProvider;
         private readonly PlayerDataModel _playerDataModel;
+        private readonly MenuPlayerController _menuPlayerController;
 
-        public CMPManager(MenuSaberProvider menuSaberProvider, PlayerDataModel playerDataModel)
+        public CMPManager(MenuSaberProvider menuSaberProvider, PlayerDataModel playerDataModel, MenuPlayerController menuPlayerController)
         {
             _menuSaberProvider = menuSaberProvider;
             _playerDataModel = playerDataModel;
+            _menuPlayerController = menuPlayerController;
         }
 
         public async void Initialize()
         {
             // Create a red left saber
-            _ = CreateSaberCustomColor(new Color(1,0,0), SaberType.SaberA);
+            //_ = CreateSaberCustomColor(new Color(1,0,0), SaberType.SaberA);
 
             // Create a left saber with whatever your left user color is
-            var secondSaber = await CreateSaberColorScheme(SaberType.SaberA);
+            var leftSaber = await CreateSaberColorScheme(SaberType.SaberA);
 
-            secondSaber.transform.position = new Vector3(-1, 0, 0);
+            leftSaber.transform.position = new Vector3(-1, 0, 0);
+
+            //_ = CreateSaberCustomColor(new Color(0, 0, 1), SaberType.SaberB);
+            var rightSaber = await CreateSaberColorScheme(SaberType.SaberB);
+
+            rightSaber.transform.localPosition = new Vector3(1, 0, 0);
         }
 
         /// <summary>
@@ -39,6 +48,7 @@ namespace CustomMenuPointers.MenuPointers
         /// Create the saber with the color of the currently selected color scheme
         /// of the user
         /// </summary>
+        
         public async Task<GameObject> CreateSaberColorScheme(SaberType saberType)
         {
             var colorScheme = _playerDataModel.playerData.colorSchemesSettings.GetSelectedColorScheme();
